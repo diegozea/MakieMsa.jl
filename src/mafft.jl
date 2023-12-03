c05c0fdaf66ba2e5264d586231891d1088f478de
Thomas Poulsen <ta.poulsen@gmail.com>
2023-12-03 18:12:24 +0100

    run and parse mafft

--- src/mafft.jl
+++ src/mafft.jl
@@ -1,7 +1,9 @@
 """
-    mafft(input_file, output_file = tempname(); options = "--maxiterate 1000 --localpair", quiet = true)
+    mafft(input_file, output_file = tempname(); options = "--auto", quiet = false)
     Run mafft on input_file with the options writing to output_file
+    set quiet = true (or add --quet to options" to suppress output.
     Return name of output_file
+    Documentation says "--maxiterate 1000 --localpair" is probably most accurate
 """
 function mafft(input_file, output_file = tempname(); options = "--auto", quiet = false)
   # High accuracy (for <~200 sequences x <~2,000 aa/nt):
@@ -23,3 +25,41 @@ function mafft(input_file, output_file = tempname(); options = "--auto", quiet =
     run(pipeline(cmd, stdout = output_file))
     output_file
 end
+
+## read msa
+using FASTX
+using BioSequences
+abstract type AbstractMsa end 
+struct MSA <: AbstractMsa
+    filename::String
+    identifiers::Vector{String}
+    sequences::Vector{String}
+end
+struct MSA_window <: AbstractMsa
+    filename::String
+    identifiers::Vector{String}
+    sequences::Vector{String}
+    window::Tuple{Int,Int} ## first position, last position
+end
+
+function read_msa(file)
+    records = collect(FASTAReader(open(file,"r")))
+    MSA(file,
+        string.(identifier.(records)),
+        string.(sequence.(records)),
+        )    
+end
+
+
+## plot an msa
+## Make function that works with static as well as observable input
+## or dispatch it on type of arguments
+function plot_msa!(ax, msa_matrix, color_matrix; hide_decorations=true)
+    
+end
+
+## slice an msa. This is used with lift to make a scrollable msa.
+function slize_msa(msa, start, end)
+    
+end
+
